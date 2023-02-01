951cc848a8fd45a1e224222824b124aef33a8596
melika760 <72795928+melika760@users.noreply.github.com>
2023-02-01 07:43:59 +0330

    add form

---
+++ src/Components/AddTasks.js
@@ -0,0 +1,25 @@
+import React,{useState} from "react";
+import Card from "./UI/Card";
+import Button from "./UI/Button";
+import classes from "./AddTasks.module.css"
+export default function AddTasks(props){
+    const[Tasks,SetTasks]=useState("")
+    function SubmitHandler(event){
+        event.preventDefault();
+        if(Tasks.trim()===0){
+            return;
+        }
+        SetTasks("")
+    }
+    function AddNewTasks(event){
+SetTasks(event.target.value)
+    }
+return(
+    <Card className={classes.input}>
+    <form onSubmit={SubmitHandler}>
+        <input type="text" placeholder="Enter new Tasks" onChange={AddNewTasks} value={Tasks}/>
+<Button type="submit">Submit</Button>
+    </form>
+    </Card>
+)
+}
